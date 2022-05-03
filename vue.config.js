78082b7ea3ee9656668688c673fe41cf5063c5d9
Krystal Tabo <krytabo@gmail.com>
2022-05-03 10:59:02 +0800

    修改內容

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
 module.exports = {
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === "production" ? "/group-buy/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/group_buy/" : "/",
 };
